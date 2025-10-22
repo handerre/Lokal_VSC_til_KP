f242dda7bf9ebbfb60082c5a78c8830f21886f15
Your Name <your.email@example.com>
2025-10-22 12:28:52 +0200

    22.10

---
+++ JS/Undervisning.js
@@ -0,0 +1,12 @@
+// /c:/Lokal_VSC_til_KP/JS/Undervisning.js
+
+
+console.log('hei verden');
+
+
+let antall = 5;
+let ganger = 10;
+let sum = (antall*ganger);
+
+console.log(antall);
+console.log(sum);
